edeedeaa6a2076185db3bdbfc7d73a8782e96307
Evan Brown <evanbrown@google.com>
2015-08-03 15:37:12 -0700

    Add version command

---
+++ commands/version.go
@@ -0,0 +1,20 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var version string
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show the version of this tool",
+}
+
+func init() {
+	versionCmd.Run = func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version %v\n", version)
+	}
+}
