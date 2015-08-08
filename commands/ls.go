1c8f8b0afda8f23032db4b03b246149e850f46b9
Evan Brown <evanbrown@google.com>
2015-08-07 22:09:50 -0700

    Write deployment state

--- commands/ls.go
+++ commands/ls.go
@@ -2,8 +2,11 @@ package commands
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/evandbrown/dm/conf"
+	"github.com/evandbrown/dm/googlecloud"
 	"github.com/evandbrown/dm/util"
 	"github.com/spf13/cobra"
 )
@@ -23,10 +26,22 @@ func init() {
 }
 
 func ls(cmd *cobra.Command, args []string) error {
+	// Get config from disk
 	config, _ := conf.ReadDeploymentConfig()
 
+	// Get service
+	service, err := googlecloud.GetService()
+	util.Check(err)
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 2, '\t', 0)
+	fmt.Fprintln(w, "Deployment Name\tProject\tState\t")
 	for _, c := range config.Deployments {
-		fmt.Printf("%s\n", c.Id)
+		call := service.Deployments.Get(c.Project, c.Id)
+		deployment, error := call.Do()
+		util.Check(error)
+		fmt.Fprintf(w, "%s\t%s\t%s\t\n", c.Id, c.Project, deployment.State)
 	}
+	w.Flush()
 	return nil
 }
